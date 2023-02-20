3c42eb22c9bba7c6a387368602f8eacc27d82c8d
vladyslav buriak <vladka910@gmail.com>
2023-02-20 16:10:51 +0200

    add path from location

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -15,7 +15,7 @@ import { InMemoryCache } from "@apollo/client";
 import { Route, Routes } from "react-router-dom";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: `http://localhost:${window.location.href}/graphql`,
 });
 const client = new ApolloClient({
   link: httpLink,
